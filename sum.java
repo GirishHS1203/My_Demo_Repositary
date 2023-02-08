9f3abc216069b4d8fec3bf3a2d966dd721afaee5
Girish <girishprince1098@gmail.com>
2023-02-08 21:11:57 +0530

    MyFirstUdemyCourse-2

--- sum.java
+++ sum.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class sum {
+import java.util.Scanner;
+public class sum {
+    public static int add(int a,int b){
+        int res= a + b;
+        System.out.println("The Sum of a and b is:"+ res);
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int a,b;
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the value of a:");
+        a=sc.nextInt();
+        System.out.println("Enter the value of b");
+        b=sc.nextInt();
+        add(a,b);
+
+
+    }
 }
