edfd717e666a63d03c4c5f17452becaded708dab
Girish <girishprince1098@gmail.com>
2023-02-11 18:20:04 +0530

    Modified and Updated

--- toCentimeters.java
+++ toCentimeters.java
@@ -4,7 +4,7 @@ public class toCentimeters {
         calcFeetAndInchesToCentimeters(50,4);
         calcFeetAndInchesToCentimeters(100);
         calcFeetAndInchesToCentimeters(156);
-       
+
 
     }
     public static double calcFeetAndInchesToCentimeters(double feet,double inches) {
